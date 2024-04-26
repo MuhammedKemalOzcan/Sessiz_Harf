a46be15d9fe13cf9aced4f127a9395e7ced4d606
mami_ <mami_@DESKTOP-H9BT6T7>
2024-04-26 21:06:21 +0300

    Add project files.

---
+++ Sessiz_Harf/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Sessiz_Harf
+{
+    internal class Program
+    {
+        //Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
+        public static bool IsConsonant(char c)
+        {
+            
+            return "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".Contains(c);
+        }
+        public static void FindConsecutiveConsonant(string text)
+        {
+            bool isConsonant = false;
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if (IsConsonant(text[i]) && IsConsonant(text[i+1]))
+                {
+                    isConsonant = true;
+                }
+            }
+            Console.WriteLine(isConsonant);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir metin giriniz:");
+            string text = Console.ReadLine();
+
+            FindConsecutiveConsonant(text);
+
+        }
+    }
+}
